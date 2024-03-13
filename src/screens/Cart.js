59e68ce4943ad51f727c5ce47a68be087129f816
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-03-13 12:53:55 +0530

    first commit

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -17,31 +17,51 @@ const Cart = ({ navigation }) => {
         <View>
             <Text style={{ color: "#000", textAlign: "center" }}>{cartProducts.length}</Text>
 
-
             <FlatList
                 data={cartProducts}
-                // numColumns={2}
-                renderItem={({ item }) => (
-                    <View>
-                        <Text style={{ color: "#000" }}>{item.title}</Text>
-                    </View>
-                )}
+                renderItem={({ item }) => {
+                    // console.log(item);
+                    return (
+                        <View style={{ flexDirection: "column", alignItems: "center", padding: 5, borderRadius: 8, backgroundColor: "#fff", marginVertical: 5, marginHorizontal: 5, elevation: 2 }}>
+                            <View style={{ flexDirection: "row" }}>
+                                <View style={{ flexDirection: "row", flex: 1.3, backgroundColor: "#f6f6f6", borderRadius: 8,padding: 3}}>
+                                    <Image source={{ uri: item.image }} style={{ resizeMode: "contain", height: 130, width: "100%" }} />
+                                </View>
+                                <View style={{ flex: 2, flexDirection: "column", justifyContent: "flex-start", height: "100%", marginLeft: 10 }}>
+                                    <Text style={{ color: "#000", fontWeight: "700", fontSize: 18, width: "100%" }}>{item.title}</Text>
+                                    <View style={{ flexDirection: "row", alignItems: "center", padding: 0 }}>
+                                        <Text style={{ color: "#948497", fontSize: 13, fontWeight: "500" }}>By</Text>
+                                        <Image source={require("../assets/nike_logo.png")} style={{ resizeMode: "contain", height: 28, width: 28, marginLeft: 3 }} />
+                                        <Text style={{ color: "#000", fontWeight: "600", fontSize: 13, }}>Nike Official</Text>
+                                        <View style={{ backgroundColor: "#3f87e7", borderRadius: 100, padding: 2, marginLeft: 5 }}>
+                                            <Icon4
+                                                name="check"
+                                                style={{
+                                                    color: '#fff',
+                                                    fontSize: 9,
+                                                    fontWeight: "600"
+                                                }}
+                                            />
+                                        </View>
+                                    </View>
+                                    <View style={{ flexDirection: "row", alignItems: "center", marginTop: 3 }}>
+                                        <TouchableOpacity style={{ backgroundColor: "#e27e45", paddingVertical: 1, borderRadius: 8, paddingHorizontal: 8, flexDirection: "row", alignItems: "center", justifyContent: "center" }}><Text style={{ fontSize: "600", fontSize: 20, color: "#fff" }}>+</Text></TouchableOpacity>
+                                        <Text style={{ color: "#000", marginLeft: 6, fontWeight: "600", fontSize: 16 }}>3</Text>
+                                        <TouchableOpacity style={{ backgroundColor: "#e27e45", borderRadius: 8, paddingHorizontal: 10, flexDirection: "row", alignItems: "center", justifyContent: "center", marginLeft: 6 }}><Text style={{ fontSize: "600", fontSize: 22, color: "#fff" }}>-</Text></TouchableOpacity>
+                                    </View>
+                                </View>
+                            </View>
+                            <View style={{ width: "100%" }}>
+                                <TouchableOpacity style={{ backgroundColor: "#b04632", paddingVertical: 9, marginTop: 8, borderRadius: 8, paddingHorizontal: 9, }} onPress={() => dispatch(removeItemFromCart(item.id))}>
+                                    <Text style={{ color: "#fff", textAlign: "center", fontWeight: "600", fontSize: 17 }}>Delete item from cart</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    )
+                }}
                 keyExtractor={(item) => item.id.toString()}
             />
 
-
-
-
-
-
-
-
-
-            {/* {cartProducts.map((item) => (
-                <View>
-                    <Text style={{ color: "#000" }}>{item.title}</Text>
-                </View>
-            ))} */}
         </View>
 
         // <View>
